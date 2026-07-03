d4c0723a71d9929e6c7a9c70bbfc38f173f1c808
artin231 <leiartrezjah@gmail.com>
2026-07-03 16:00:50 +0330

    changing js

--- js/script.js
+++ js/script.js
@@ -6,36 +6,46 @@ let hb = document.getElementById("h");
 let mb = document.getElementById("m");
 let sb = document.getElementById("s");
 
+clicked = false;
 let play = () => {
+    var int;
     h = Number(hb.innerHTML);
     m = Number(mb.innerHTML);
     s = Number(sb.innerHTML);
-    setInterval(() => {
+    let run = () => {
 
-        if (s > 0) {
-            s--;
-        }
-        else {
-            if (m > 0) {
-                m--;
-                s = 59;
+            if (s > 0) {
+                s--;
             }
             else {
-                if (h > 0) {
-                    h--;
-                    m = 59;
+                if (m > 0) {
+                    m--;
                     s = 59;
                 }
                 else {
-                    return;
+                    if (h > 0) {
+                        h--;
+                        m = 59;
+                        s = 59;
+                    }
+                    else {
+                        return;
+                    }
                 }
             }
-        }
 
-        sb.innerHTML = s;
-        mb.innerHTML = m;
-        hb.innerHTML = h;
+            sb.innerHTML = s;
+            mb.innerHTML = m;
+            hb.innerHTML = h;
+    }
+    if (!clicked) {
+        clicked = true;
+        int = setInterval(run, 1000);
+    }
+    else{
+        location.reload()
+    }
 
-    }, 1000);
+
 }
 
