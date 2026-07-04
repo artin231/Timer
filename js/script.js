3836e00750075a2678738e3d9f852761647e9e28
artin231 <leiartrezjah@gmail.com>
2026-07-04 10:20:16 +0330

    i fixed a bug in the timer and now the timer does not reset

--- js/script.js
+++ js/script.js
@@ -7,24 +7,27 @@ let mb = document.getElementById("m");
 let sb = document.getElementById("s");
 let btn = document.getElementById('play');
 clicked = false;
+st = {'click':false,remain_h:Number(hb.innerHTML),remain_m:Number(mb.innerHTML),remain_s:Number(sb.innerHTML),int:null};
+let min = 1;
 let play = () => {
     var int;
     h = Number(hb.innerHTML);
     m = Number(mb.innerHTML);
     s = Number(sb.innerHTML);
+    min = 1;
     let run = () => {
 
             if (s > 0) {
-                s--;
+                s-=min;
             }
             else {
                 if (m > 0) {
-                    m--;
+                    m-=min;
                     s = 59;
                 }
                 else {
                     if (h > 0) {
-                        h--;
+                        h-=min;
                         m = 59;
                         s = 59;
                     }
@@ -38,13 +41,18 @@ let play = () => {
             mb.innerHTML = m;
             hb.innerHTML = h;
     }
-    if (!clicked) {
+    if (!st.clicked) {
         btn.innerHTML = 'stop';
-        clicked = true;
-        int = setInterval(run, 1000);
+        st.clicked = true;
+        st.int = setInterval(run, 1000);
     }
     else{
-        location.reload()
+        clearInterval(st.int);
+        st.int = null;
+        st.clicked = false;
+        min = null;
+        btn.innerHTML = 'start'
+
     }
 
 
